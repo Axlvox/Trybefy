99bd2dc8af50323468aece1bcd57be0b5650ec36
Axlvox <leandro.jlds@gmail.com>
2024-02-29 02:52:53 -0300

    Remoção do PLano Trimestral

---
+++ src/07_challenge.sql
@@ -0,0 +1,14 @@
+UPDATE users
+SET plan_id = (
+    SELECT id
+    FROM plans
+    WHERE name = 'Mensal'
+)
+WHERE plan_id = (
+    SELECT id
+    FROM plans
+    WHERE name = 'Trimestral'
+);
+
+DELETE FROM plans
+WHERE name = 'Trimestral';
