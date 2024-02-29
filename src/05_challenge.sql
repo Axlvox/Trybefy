5891d6da0b66b409ac312c06f3f83b61cdf0b83f
Axlvox <leandro.jlds@gmail.com>
2024-02-29 02:46:22 -0300

    Correção dos nomes da tabela

--- src/05_challenge.sql
+++ src/05_challenge.sql
@@ -1,4 +1,4 @@
-SELECT full_name AS `Nome`, birthday AS `Data de Nascimento`
+SELECT full_name AS `Nome completo`, birthday AS `Data de nascimento`
 FROM users
 WHERE active = true
 AND YEAR(birthday) >= 1990;
