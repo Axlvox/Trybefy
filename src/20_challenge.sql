e95b06d05dc7af4959e5807faf2acc03ac126144
Axlvox <leandro.jlds@gmail.com>
2024-02-29 04:25:43 -0300

    Retorna Artistas e Álbuns, exceto The Beatles

---
+++ src/20_challenge.sql
@@ -0,0 +1,14 @@
+SELECT 
+    a.name AS Artista,
+    al.title AS Álbum,
+    s.title AS Música
+FROM 
+    artists a
+JOIN 
+    albums al ON a.id = al.artist_id
+JOIN 
+    songs s ON al.id = s.album_id
+WHERE 
+    a.name != 'The Beatles'
+ORDER BY 
+    a.name, al.title, s.title;
